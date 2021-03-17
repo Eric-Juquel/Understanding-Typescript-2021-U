7d95cdf2b3c6bdf4b7ab491dd29f790c8364c672
Eric J <ejuquel@yahoo.fr>
2021-03-17 18:01:11 +0100

    Decorators

--- generics/src/app.ts
+++ generics/src/app.ts
@@ -1,1 +1,110 @@
-// Code goes here!
+const names: Array<string> = [];
+
+const promise: Promise<string> = new Promise((resolve) => {
+  setTimeout(() => {
+    resolve('This is done!');
+  }, 2000);
+});
+
+promise.then((data) => {
+  data.split(' ');
+});
+
+// Creating Generics //
+
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+const mergedObj = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
+const mergedObj2 = merge({ name: 'Eric', human: true }, { age: 50, sex: 'M' });
+
+console.log(mergedObj.age);
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = 'Got no value.';
+  if (element.length === 1) {
+    descriptionText = 'Got 1 element.';
+  } else if (element.length > 1) {
+    descriptionText = 'Got ' + element.length + ' elements';
+  }
+  return [element, descriptionText];
+}
+
+console.log(countAndDescribe('Hello there !'));
+
+function extractAndConvert<T extends object, U extends keyof T>(
+  obj: T,
+  key: U
+) {
+  return 'Value: ' + obj[key];
+}
+
+extractAndConvert({ name: 'Max' }, 'name');
+
+// Generic Classes
+
+class DataStorage<T extends string | number | boolean> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    if (this.data.indexOf(item) === -1) {
+      return;
+    }
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItems() {
+    return [...this.data];
+  }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem('Max');
+textStorage.addItem('Manu');
+textStorage.removeItem('Max');
+console.log(textStorage.getItems());
+
+const numberStorage = new DataStorage<number>();
+
+/// Works only with primitive values and not objects ///
+
+// const objectStorage = new DataStorage<object>();
+// objectStorage.addItem({ name: 'Max' });
+// objectStorage.addItem({ name: 'Manu' });
+
+// objectStorage.removeItem({ name: 'Max' });
+// console.log(objectStorage.getItems());
+
+
+// Partial //
+interface CourseGoal {
+  title: string;
+  description: string;
+  completeUntil: Date;
+}
+
+function createCourseGoal(
+  title: string,
+  description: string,
+  date: Date
+): CourseGoal {
+  let courseGoal: Partial<CourseGoal> = {};
+  courseGoal.title = title;
+  courseGoal.description = description;
+  courseGoal.completeUntil = date;
+  return courseGoal as CourseGoal
+}
+
+// Readonly  //
+
+const firstnames: Readonly<string[]> = ['Max', 'Anna']
+// firstnames.push('Manu')
